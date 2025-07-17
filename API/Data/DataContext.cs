3daf524c01742b0ed9d9f8cd61052db9a6f24fb5
InfoTCube <infocube.yout@gmail.com>
2025-07-17 19:27:26 +0200

    feat: API initialization

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+}
